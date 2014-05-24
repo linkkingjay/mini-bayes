55d50097f70a4602dc9a0322c7c85b1fc66f404e
linkkingjay <linkkingjay@gmail.com>
2014-05-24 09:56:54 +0800

    added some comments

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var fs = require('fs');
 var csv = require('csv');
 
 var DIR = 'data/';
@@ -8,52 +7,67 @@ function Bayes(fileName) {
 }
 
 Bayes.prototype.classify = function (X, fn) {
-  csv().from.path(DIR + this.fileName).to.array(function (arrayData) {
+  // 从 csv 文件读取数据转换为数组，data 是一个二维数组，作为回调函数的一个参数
+  csv().from.path(DIR + this.fileName).to.array(function (data) {
+    //
+    // 类别对象集合
+    // @name: 名称
+    // @count: 数量
+    // @probability: 先验概率 P(Y)
+    // @xProbability: 类条件概率 P(X|Y)
+    // @p: 先验概率与类条件概率的乘积，即最终用来确认哪个分类的概率
+    //
     var classSet = [];
+    // 训练集
     var trainedSet = [];
 
-    var n = arrayData.length;
-    var m = arrayData[0].length;
+    var rows = data.length;
+    var columns = data[0].length;
+
     // 遍历每一行数据
-    for (var i = 1; i < n; i++) {
+    for (var i = 1; i < rows; i++) {
       // 判断是否存在此类，如果存在则更新对应项的数量
       for (var j = 0; j < classSet.length; j++) {
-        if (arrayData[i][m - 1] === classSet[j].name) {
+        if (data[i][columns - 1] === classSet[j].name) {
           classSet[j].count++;
           break;
         }
       }
       // 否则添加新的类项
       if (j === classSet.length) {
-        classSet.push({ name: arrayData[i][m - 1], count: 1 });
+        classSet.push({ name: data[i][columns - 1], count: 1 });
       }
     }
 
-    trainedSet = arrayData.slice(1, n - 1);
+    // 训练集从第一行开始到最后一行
+    trainedSet = data.slice(1, rows - 1);
 
     // 计算各个分类的概率
     classSet.forEach(function (classItem) {
       // 计算训练集中每个类别的概率
-      classItem.probability = classItem.count / (n - 1);
+      classItem.probability = classItem.count / (rows - 1);
       classItem.xProbability = classItem.probability;
 
       // 计算 X 属于类别 classItem 的概率
       X.forEach(function (x, index) {
         var count = 0;
         trainedSet.forEach(function (dataItem) {
-          if (dataItem[m - 1] === classItem.name && dataItem[index] === x) {
+          if (dataItem[columns - 1] === classItem.name && 
+              dataItem[index] === x) {
             count++
           }
         });
         // 把 Xi 乘到最终的结果中去
         classItem.xProbability *= count / classItem.count;
       });
+
+      classItem.p = classItem.probability * classItem.xProbability;
     });
 
     // 计算概率值最大的分类
     var maxIndex = 0;
     for (var i = 1; i < classSet.length; i++) {
-      if (classSet[i].xProbability > classSet[maxIndex].xProbability) {
+      if (classSet[i].p > classSet[maxIndex].p) {
         maxIndex = i;
       }
     }
@@ -61,6 +75,7 @@ Bayes.prototype.classify = function (X, fn) {
     fn(X, classSet[maxIndex]);
   });
 }
+
 Bayes.prototype.test = function (x) {
   this.classify(x, function (X, classItem) {
     console.log("X:", X);
